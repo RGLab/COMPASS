f55428bfb0cd970acee7e58a67bc16f0d51c970c
Kevin Ushey <kevinushey@gmail.com>
2013-11-04 11:28:32 -0800

    S3 generic for scores methods
    - can be called on COMPASSResult, or matrix

--- R/Scores.R
+++ R/Scores.R
@@ -3,13 +3,27 @@
 ##' Computes the functionality score for each observation from the gamma matrix
 ##' of a COMPASS model fit.
 ##' 
-##' @param CR An object of class \code{COMPASSResult}, as returned by
-##'   \code{\link{COMPASS}}.
+##' @param x An object of class \code{COMPASSResult}, as returned by
+##'   \code{\link{COMPASS}}. Alternatively, a matrix of functionality scores.
 ##' @return A numeric vector of functionality scores.
 ##' @export
-FunctionalityScore <- function(CR) {
-  M <- CR$fit$mean_gamma
-  Fscore <- rowMeans(M[, -c(ncol(M))])
+FunctionalityScore <- function(x) {
+  UseMethod("FunctionalityScore")
+}
+
+##' @rdname FunctionalityScore
+##' @method FunctionalityScore COMPASSResult
+##' @S3method FunctionalityScore COMPASSResult
+FunctionalityScore.COMPASSResult <- function(x) {
+  x <- x$fit$mean_gamma
+  NextMethod("FunctionalityScore")
+}
+
+##' @rdname FunctionalityScore
+##' @method FunctionalityScore default
+##' @S3method FunctionalityScore default
+FunctionalityScore.default <- function(x) {
+  Fscore <- rowMeans(x[, -c(ncol(x))])
   return(Fscore)
 }
 
@@ -18,22 +32,28 @@ FunctionalityScore <- function(CR) {
 ##' Computes the Polyfunctionality score for each observation from the 
 ##' gamma matrix of a COMPASS model fit.
 ##' 
-##' @param CR An object of class \code{COMPASSResult}, as returned by
+##' @param x An object of class \code{COMPASSResult}, as returned by
 ##'   \code{\link{COMPASS}}.
 ##' @param normalization A \code{character} vector specifying how the score
 ##'  is to be normalized. Either using \code{"all"} possible categories, or 
 ##'  just the \code{"observed"} categories. Defaults to \code{"all"} categories.
 ##' @return a \code{vector} of polyfunctionality scores.
 ##' @export
-PolyfunctionalityScore <- function(CR, normalization=c("all","observed")) {
-  M <- CR$fit$mean_gamma
-  degree <- rev(rev(CR$fit$categories[,ncol(CR$fit$categories)])[-1L])
-  normalization <- match.arg(arg=normalization,choices=c("all","observed"))
+PolyfunctionalityScore <- function(x, degree, normalization) {
+  UseMethod("PolyfunctionalityScore")
+}
+
+##' @rdname PolyfunctionalityScore
+##' @method PolyfunctionalityScore COMPASSResult
+##' @S3method PolyfunctionalityScore COMPASSResult
+PolyfunctionalityScore.COMPASSResult <- function(x, degree, normalization="all") {
+  M <- x$fit$mean_gamma
+  degree <- rev(rev(x$fit$categories[,ncol(x$fit$categories)])[-1L])
   switch(normalization,
     all = {
       norm <- choose(
-        ncol(CR$fit$categories)-1, 
-        1:(ncol(CR$fit$categories)-1)
+        ncol(x$fit$categories)-1, 
+        1:(ncol(x$fit$categories)-1)
       )
     },
     observed = {
@@ -44,4 +64,11 @@ PolyfunctionalityScore <- function(CR, normalization=c("all","observed")) {
   PFscore <- M[,-ncol(M)] %*% degree
   PFscore <- setNames( as.numeric(PFscore), rownames(PFscore) )
   return(PFscore)
+}
+
+##' @rdname PolyfunctionalityScore
+##' @method PolyfunctionalityScore COMPASSResult
+##' @S3method PolyfunctionalityScore COMPASSResult
+PolyfunctionalityScore.default <- function(x, degree, normalization) {
+  return( rowMeans(as.matrix(x) * matrix( rep(degree, each=nrow(x)), nrow=nrow(x))) )
 }
