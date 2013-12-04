6503beec0b0ed24e402cad6e5be2da23e8522b9b
Kevin Ushey <kevinushey@gmail.com>
2013-12-04 15:45:52 -0800

    speed up the counts extraction in CCFGS

--- R/GatingSetToCOMPASS.R
+++ R/GatingSetToCOMPASS.R
@@ -1,93 +1,8 @@
 ## debugging
 if (FALSE) {
   library(flowWorkspace)
-  gs <- load_gs("../shinyGate/gs/LoveLab-Gated")
+  gs <- load_gs("../shinyGate/gs/LoveLab")
 }
-
-##'  Extract A GatingSet Node to a COMPASSContainer
-##'  
-##'  This function can be used to take gated data from a \code{flowWorkspace}
-##'  object (e.g., a \code{GatingSet}), and retrieve it as a 
-##'  \code{COMPASSContainer}, suitable for ##'  modelling with \code{COMPASS}.
-##'  
-##'  @param gs A \code{GatingSet} object.
-##'  @param node The name of the node. We extract all children of that node,
-##'    under the assumption that the markers of interest have been gated at
-##'    the chosen node.
-##'  @param children A character vector, or list, giving the names of the children of
-##'    \code{node} that we wish to extract. If unspecified, we take all
-##'    children of that node. If the character vector passed is named, we map
-##'    from value to name; e.g. if a node is named \code{154} but we prefer the
-##'    name \code{CD154+}, we could pass \code{`CD154+`='154'}.
-##'  @param meta A \code{data.frame} of meta data. If it is missing, we attempt
-##'    to pull the metadata from the \code{GatingSet}, and throw an error if it
-##'    is unavailable.
-##'  @param individual_id The name of the vector in \code{meta} that denotes the
-##'    individuals from which samples were drawn. 
-##'  @param sample_id The name of the vector in \code{meta} that denotes the samples.
-##'    This vector should contain all of the names in the \code{data} input.
-##'  @export
-##'  @seealso \code{\link{COMPASSContainer}}
-GatingSetToCOMPASS <- function(gs, node, children, meta, individual_id, 
-                               sample_id) {
-  
-  if (require(flowWorkspace)) {
-    
-    if (!inherits(gs, "GatingSet")) {
-      stop("'gs' must be an object of class 'GatingSet'")
-    }
-    
-    if (missing(children)) {
-      children <- getChildren(gs[[1]])
-    }
-    
-    doWarn <- FALSE
-    .children <- getChildren(gs[[1]], node, isPath = TRUE)
-    for (i in 1:length(gs)) {
-      if (!identical(.children, getChildren(gs[[i]], node, isPath = TRUE))) {
-        doWarn <- TRUE
-      }
-    }
-    
-    if (doWarn) {
-      warning("The children of node '", node, "' are not identical across ", 
-              "each of the samples encapsulated by your GatingSet; be sure that ", 
-              "your samples have been gated uniformly at each node.")
-    }
-    
-    ## get the data at each node
-    data <- vector("list", length(children))
-    for (i in 1:length(gs)) {
-      data[[i]] <- getData(gs, children[i])
-    }
-    data <- getData(gs, node)
-    intensities <- Map(exprs, data)
-    names(intensities) <- sampleNames(gs)
-    counts <- integer(length(gs))
-    for (i in seq_along(counts)) {
-      counts[[i]] <- getTotal(gs[[i]], node, flowJo = FALSE)
-    }
-    names(counts) <- sampleNames(gs)
-    
-    ## try to get the metadata, if available
-    if (missing(meta)) {
-      tryCatch(meta <- pData(gs), error = function(e) {
-        stop("No metadata available!")
-      })
-    }
-    
-    CC <- COMPASSContainer(data = intensities, counts = counts, meta = meta, 
-                           individual_id = individual_id, sample_id = sample_id)
-    
-    return(CC)
-    
-  } else {
-    stop("This function requires 'flowWorkspace' to be installed; ", 
-         "try running 'biocLite(\"flowWorkspace\")' to install.")
-  }
-  
-}
-
 
 ##' Create a COMPASS Container from a GatingSet
 ##' 
@@ -133,12 +48,10 @@ COMPASSContainerFromGatingSet <- function(gs = NULL, node = NULL, filter.fun = N
                                           mp = NULL, countFilterThreshold = 5000, matchmethod = c("regex", "Levenshtein"), 
                                           markers = NA) {
   if (require(flowWorkspace)) {
+    
     if (is.null(gs) | is.null(node)) {
       stop("Must specify a gating set and parent node.")
     }
-    # extract all the counts
-    message("Extracting cell counts")
-    stats <- getPopStats(gs, statistic = "count")
     
     pd <- pData(gs)
     # Do the expected columns exist?
@@ -161,8 +74,24 @@ COMPASSContainerFromGatingSet <- function(gs = NULL, node = NULL, filter.fun = N
                    node))
     }
     
-    # Grab the counts for the parent
-    counts <- stats[which(rownames(stats) %in% parent.pop), ]
+    # extract all the counts
+    message("Extracting cell counts")
+    .get_cell_counts <- function(gs, samples, node) {
+      ind <- .Call("R_getNodeID", gs@pointer, samples[[1]], node)
+      output <- unlist( lapply(samples, function(x) {
+        .Call("R_getPopStats", gs@pointer, x, ind)$FlowCore["count"]
+      }) )
+      names(output) <- samples
+      return(output)
+    }
+    
+    if (inherits(gs, "GatingSetList")) {
+      counts <- unlist( lapply(gs@data, function(x) {
+        .get_cell_counts(x, sampleNames(x), node)
+      }))
+    } else if (inherits(gs, "GatingSet")) {
+      counts <- .get_cell_counts(gs, sampleNames(gs), node)
+    }
     
     # Extract the parent node name from the full population name
     parent.node <- laply(strsplit(parent.pop, "/"), function(x) x[length(x)])
