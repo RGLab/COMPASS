dcf8e5912b80f0900c589124fcf9e0f6030ddc6b
k.ushey <k.ushey@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-04-04 19:33:48 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 2 commit(s).
    Commit information:
    
        Commit id: a77bbd32f9b1665bb3a0c7e4ed2c8d92509b4864
        Commit message:
        One more unit test
        Committed by kevinushey <kevinushey at gmail.com>
        Commit date: 2014-04-04T12:33:29-07:00
    
        Commit id: b7396ae1d8ab4e3ce8701225feacde97215fb529
        Commit message:
        Clean out the continuous model files (not used)
        Committed by kevinushey <kevinushey at gmail.com>
        Commit date: 2014-04-04T12:33:29-07:00
    
    From: Bioconductor Git-SVN Bridge <bioc-sync@bioconductor.org>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/COMPASS@88439 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/COMPASS_init.c
+++ src/COMPASS_init.c
@@ -10,7 +10,6 @@ SEXP melt_matrix( SEXP x );
 SEXP transpose_list(SEXP x);
 SEXP COMPASS_CellCounts_character(SEXP dataSEXP, SEXP combinationsSEXP);
 SEXP COMPASS_CellCounts(SEXP xSEXP, SEXP combosSEXP);
-SEXP model(SEXP T, SEXP I, SEXP K, SEXP M, SEXP ttt, SEXP SS, SEXP alpha_u, SEXP alpha_s, SEXP mu_u, SEXP mu_s, SEXP alpha, SEXP beta, SEXP gamma, SEXP n_s, SEXP n_u, SEXP varp_u, SEXP lambda_u,SEXP indi, SEXP d, SEXP ybar_s, SEXP ybar_u, SEXP ys2_s, SEXP ys2_u, SEXP a, SEXP b, SEXP lambda, SEXP mk, SEXP Istar, SEXP mKstar, SEXP pp, SEXP pb1, SEXP pb2, SEXP lambda_s,SEXP var_1, SEXP var_2, SEXP p_var, SEXP p_vars, SEXP var_1s, SEXP var_2s, SEXP m_s,SEXP Sigma_s, SEXP p_varu, SEXP var_1u,SEXP var_2u, SEXP m_u, SEXP Sigma_u, SEXP p_vara, SEXP var_1a, SEXP var_2a, SEXP sig_alpha1, SEXP alpha1, SEXP p_varb, SEXP var_1b, SEXP var_2b, SEXP sig_beta1, SEXP beta1, SEXP A_alphau, SEXP A_alphas, SEXP A_gm, SEXP A_mus, SEXP A_muu, SEXP A_alpha, SEXP A_beta, SEXP Tune, SEXP pgamma);
 SEXP samplePuPs(SEXP alphau, SEXP alphas, SEXP gammat, SEXP T, SEXP K , SEXP nsi, SEXP nui, SEXP d, SEXP M);
 SEXP updatealphas_Exp(SEXP alphast,SEXP n_s, SEXP K, SEXP I, SEXP lambda_s, SEXP gammat, SEXP var_1,SEXP var_2, SEXP p_var, SEXP ttt);
 SEXP updatealphau_noPu_Exp(SEXP alphaut, SEXP n_s, SEXP n_u, SEXP I, SEXP K, SEXP lambda_u, SEXP var_p, SEXP ttt, SEXP gammat);
@@ -22,7 +21,6 @@ R_CallMethodDef callMethods[]  = {
   {"C_transpose_list", (DL_FUNC) &transpose_list, 1},
   {"C_COMPASS_CellCounts_character", (DL_FUNC) &COMPASS_CellCounts_character, 2},
   {"C_COMPASS_CellCounts", (DL_FUNC) &COMPASS_CellCounts, 2},
-  {"C_model", (DL_FUNC) &model, 65},
   {"C_samplePuPs", (DL_FUNC) &samplePuPs, 9},
   {"C_updatealphas_Exp", (DL_FUNC) &updatealphas_Exp, 10},
   {"C_updatealphau_noPu_Exp", (DL_FUNC) &updatealphau_noPu_Exp, 9},
