0eaca252902dd3956d052fa49da5eb5274729636
k.ushey <k.ushey@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-04-04 21:06:15 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 2 commit(s).
    Commit information:
    
        Commit id: 9d730da99aba1fcc1257da41f878111acca95074
        Commit message:
        Add cleanup script
        Committed by kevinushey <kevinushey at gmail.com>
        Commit date: 2014-04-04T14:05:56-07:00
    
        Commit id: a9264b370a7d9e8da4507fd5fd13b90100eef588
        Commit message:
        Add cleanup script; it will ensure builds are fater
        Committed by kevinushey <kevinushey at gmail.com>
        Commit date: 2014-04-04T14:05:56-07:00
    
    From: Bioconductor Git-SVN Bridge <bioc-sync@bioconductor.org>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/COMPASS@88445 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/cleanup.R
@@ -0,0 +1,57 @@
+## This function:
+## 1. Concatenates all .c files into <pkg>_c.c,
+## 2. Concatenates all .cpp files into <pkg>_cpp.cpp,
+## 3. But leaves RcppExports.cpp and <pkg>_init.c unmodified.
+## This greatly speeds compilation time.
+cleanup <- function() {
+  
+  ## Bail if this is an R CMD INSTALL
+  if (Sys.getenv("R_INSTALL_PKG") != "") {
+    message("It looks like this isn't being called from R CMD build; bailing out")
+    return(invisible(NULL))
+  }
+  
+  files <- list.files()
+  stopifnot("DESCRIPTION" %in% files)
+  
+  ## get the package name from the DESCRIPTION file
+  DESCRIPTION <- as.list( read.dcf("DESCRIPTION")[1, ] )
+  pkg_name <- DESCRIPTION$Package
+  pkg_version <- DESCRIPTION$Version
+  
+  ## copy the files to a 'build' directory
+  buildDir <- getwd()
+  
+  ## in the build directory, 'cat' all the src files together
+  ## have a separate file for .c, .cpp files
+  src_files <- list.files( full.names=TRUE,
+    file.path( buildDir, "src" )
+  )
+  
+  ## but don't concatenate init.c; copy it separately
+  src_files <- grep("init.c", src_files, value=TRUE, fixed=TRUE, invert=TRUE)
+  
+  ## regex: the regex to match for picking up files
+  ## ext: the file extension to use on the outputted file
+  concatenate_src <- function(regex, ext) {
+    files <- shQuote(grep(regex, src_files, value=TRUE))
+    final <- shQuote( paste( sep='', buildDir, "/src/", pkg_name, "_", gsub("\\.", "", ext), ext ) )
+    system( paste("touch", final) )
+    files <- files[ files != final ]
+    for( file in files ) {
+      system( paste("cat", file, ">>", final) )
+      system( paste("echo '' >>", final ) ) ## adds a newline just in case
+      system( paste("rm", file) )
+    }
+  }
+  
+  cat("Concatenating source files...\n")
+  concatenate_src("\\.c$", ".c")
+  concatenate_src("\\.cpp$", ".cpp")
+  
+  ## remove all the .o, .so, .dll files
+  for( file in grep("o$|so$|dll$", list.files( full.names=TRUE, file.path( buildDir, "src" ) ), value=TRUE ) ) {
+    file.remove(file)
+  }
+  
+}
