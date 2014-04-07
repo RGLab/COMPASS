78ebf18cd7ea3ce01b68f8af7097886c0de74237
kevinushey <kevinushey@gmail.com>
2014-04-07 16:37:56 -0700

    Normalize by total number of possible subsets in all cases

--- R/Scores.R
+++ R/Scores.R
@@ -1,11 +1,14 @@
 ##' Compute the Functionality Score for each subject fit in a COMPASS model
 ##'
 ##' Computes the functionality score for each observation from the gamma matrix
-##' of a COMPASS model fit. The scores are normalized to one.
+##' of a COMPASS model fit. The scores are normalized according to the total
+##' number of possible subsets that could be observed (\code{2^M - 1}).
 ##'
 ##' @param x An object of class \code{COMPASSResult}, as returned by
 ##'   \code{\link{COMPASS}}. Alternatively, a matrix of functionality scores,
 ##'   used under the assumption that the 'null' category has been dropped.
+##' @param n The number of markers included in an experiment. It is inferred
+##'   from the data when \code{x} is a \code{COMPASSResult}.
 ##' @return A numeric vector of functionality scores.
 ##' @export
 ##' @examples
@@ -13,22 +16,27 @@
 ##' @note The null category is implicitly dropped when computing the functionality
 ##'   score for a \code{COMPASS} result; this is not true for the regular matrix
 ##'   method.
-FunctionalityScore <- function(x) {
+FunctionalityScore <- function(x, n) {
   UseMethod("FunctionalityScore")
 }
 
 ##' @rdname FunctionalityScore
 ##' @export
-FunctionalityScore.COMPASSResult <- function(x) {
+FunctionalityScore.COMPASSResult <- function(x, n) {
   ## we drop the last column as it is the 'NULL' category
+  n <- ncol(x$fit$categories) - 1
   x <- x$fit$mean_gamma[, -ncol(x$fit$mean_gamma)]
-  NextMethod("FunctionalityScore")
+  apply(x, 1, function(row) {
+    sum(row) / (2^n - 1)
+  })
 }
 
 ##' @rdname FunctionalityScore
 ##' @export
-FunctionalityScore.default <- function(x) {
-  return( rowMeans(x) )
+FunctionalityScore.default <- function(x, n) {
+  apply(x, 1, function(row) {
+    sum(row) / (2^n - 1)
+  })
 }
 
 ##' Compute the Polyfunctionality Score for each subject fit in a COMPASS model
@@ -42,54 +50,33 @@ FunctionalityScore.default <- function(x) {
 ##' @param degree A vector of weights. If missing, this is the 'degree of
 ##' functionality', that is, the number of markers expressed in a particular
 ##' category.
-##' @param normalization A \code{character} vector specifying how the score
-##'  is to be normalized. Either using \code{"all"} possible categories, or
-##'  just the \code{"observed"} categories. Defaults to \code{"all"} categories.
 ##' @param n The total number of markers. This is inferred when \code{x} is a
-##'   \code{COMPASSResult}, and hence this argument is unused.
+##'   \code{COMPASSResult}, and is unused in that case.
 ##' @return A numeric vector of polyfunctionality scores.
 ##' @export
 ##' @examples
 ##' PolyfunctionalityScore(CR)
-PolyfunctionalityScore <- function(x, degree, normalization, n) {
+PolyfunctionalityScore <- function(x, degree, n) {
   UseMethod("PolyfunctionalityScore")
 }
 
 ##' @rdname PolyfunctionalityScore
 ##' @export
-PolyfunctionalityScore.COMPASSResult <- function(x, degree, normalization="all", n) {
-  M <- x$fit$mean_gamma
-  if (missing(degree)) {
-    degree <- x$fit$categories[,ncol(x$fit$categories)]
-    degree <- degree[ -length(degree) ] ## drop the NULL category
-  }
-  switch(normalization,
-    all = {
-      norm <- choose(
-        ncol(x$fit$categories)-1,
-        1:(ncol(x$fit$categories)-1)
-      )
-    },
-    observed = {
-      norm <- unname(c(table(degree)[degree]))
-    }
-  )
-  degree <- degree / norm[degree]
-  PFscore <- M[, -ncol(M)] %*% degree
-  PFscore <- setNames( as.numeric(PFscore), rownames(PFscore) )
-
-  ## divide by number of markers
-  PFscore <- PFscore / (ncol(x$data$categories)-1)
-
-  ## normalize to 1
-  PFscore <- PFscore * 2 / (ncol(x$data$categories))
-  return(PFscore)
+PolyfunctionalityScore.COMPASSResult <- function(x, degree, n) {
+  degree <- x$fit$categories[, "Counts"]
+  n <- ncol(x$fit$categories) - 1
+  x <- x$fit$mean_gamma
+  apply(x, 1, function(row) {
+    ## (2 / (n+1)) is a factor that normalized the score between 0 and 1
+    sum(row * degree / choose(n, degree)) / n * (2 / (n + 1))
+  })
 }
 
 ##' @rdname PolyfunctionalityScore
 ##' @export
-PolyfunctionalityScore.default <- function(x, degree, normalization, n) {
+PolyfunctionalityScore.default <- function(x, degree, n) {
   apply(x, 1, function(row) {
+    ## (2 / (n+1)) is a factor that normalized the score between 0 and 1
     sum(row * degree / choose(n, degree)) / n * (2 / (n + 1))
   })
 }
--- tests/testthat/test-FunctionalityScore.R
+++ tests/testthat/test-FunctionalityScore.R
@@ -3,22 +3,26 @@ context("FS, PFS")
 test_that("We compute the functionality, polyfunctionality scores correctly for matrices", {
 
   m <- matrix(1, 5, 8)
-  degree <- c(1, 1, 1, 2, 2, 2, 3, 0)
+
+  ## Remove null category
+  m <- m[, -ncol(m)]
 
   expect_equal(
-    FunctionalityScore(m),
+    FunctionalityScore(m, 3),
     rep(1, 5)
   )
+
+  degree <- c(1, 1, 1, 2, 2, 2, 3)
 
   expect_equal(
     PolyfunctionalityScore(m, degree, n = 3),
     rep(1, 5)
   )
 
-  m <- matrix( c(1, 1, 1, 0, 0, 0, 0, 0), 5, 8, byrow=TRUE )
+  m <- matrix( c(1, 1, 1, 0, 0, 0, 0), 5, 7, byrow=TRUE )
   expect_equal(
-    FunctionalityScore(m),
-    rep(3/8, 5)
+    FunctionalityScore(m, n=3),
+    rep(3/7, 5)
   )
 
   expect_equal(
@@ -34,7 +38,7 @@ test_that("We compute the functionality, polyfunctionality scores correctly for 
   CRC$fit$mean_gamma <- matrix(1, 5, 8)
 
   expect_equal(
-    FunctionalityScore(CRC),
+    FunctionalityScore(CRC, n = 3),
     rep(1, 5)
   )
 
