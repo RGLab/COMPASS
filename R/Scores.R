ba0f81c1384ac6ed20387b92cbfb246385f8813a
g.finak <g.finak@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-06-01 17:52:04 +0000

    Upadate FS and PFS to allow markers argument to compute scores from a subset of markers. The scores function now accepts it as well.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/COMPASS@118065 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/Scores.R
+++ R/Scores.R
@@ -9,6 +9,7 @@
 ##'   used under the assumption that the 'null' category has been dropped.
 ##' @param n The number of markers included in an experiment. It is inferred
 ##'   from the data when \code{x} is a \code{COMPASSResult}.
+##' @param markers The set of markers for which to compute a Functionality score. By default uses all markers. Must match names returned by \code{markers()}.
 ##' @return A numeric vector of functionality scores.
 ##' @export
 ##' @examples
@@ -16,24 +17,41 @@
 ##' @note The null category is implicitly dropped when computing the functionality
 ##'   score for a \code{COMPASS} result; this is not true for the regular matrix
 ##'   method.
-FunctionalityScore <- function(x, n) {
+##' @import pdist
+FunctionalityScore <- function(x, n,markers=NULL) {
   UseMethod("FunctionalityScore")
 }
 
 ##' @rdname FunctionalityScore
 ##' @export
-FunctionalityScore.COMPASSResult <- function(x, n) {
+FunctionalityScore.COMPASSResult <- function(x, n,markers=NULL) {
   ## we drop the last column as it is the 'NULL' category
   n <- ncol(x$fit$categories) - 1
-  x <- x$fit$mean_gamma[, -ncol(x$fit$mean_gamma)]
-  apply(x, 1, function(row) {
+  y <- x$fit$mean_gamma[, -ncol(x$fit$mean_gamma)]
+  fs = apply(y, 1, function(row) {
     sum(row) / (2^n - 1)
   })
+  #If markers was given, we compute a functionality score based on the subset of markers
+  if(!is.null(markers)){
+    if(!all(markers%in%markers(x))){
+      stop("Invalid marker names")
+    }
+    message("Computing a Functionality Score based on ",paste(markers,collapse=", "))
+    new_categories = unique(categories(x,FALSE)[,markers,drop=FALSE])
+    all_categories=categories(x,FALSE)[,markers,drop=FALSE]
+    dmat = as.matrix(pdist(new_categories,all_categories))
+    cat_indices = apply(dmat,1,function(y)which(y==0))
+    new_mean_gamma=sapply(cat_indices,function(i)apply(Gamma(x)[,i,],1,mean))
+    new_scores = rowSums(new_mean_gamma)
+    new_scores=new_scores/(2^length(markers)-1)
+    fs=new_scores
+  }
+  return(fs)
 }
 
 ##' @rdname FunctionalityScore
 ##' @export
-FunctionalityScore.default <- function(x, n) {
+FunctionalityScore.default <- function(x, n,markers=NULL) {
   apply(x, 1, function(row) {
     sum(row) / (2^n - 1)
   })
@@ -52,29 +70,49 @@ FunctionalityScore.default <- function(x, n) {
 ##' category.
 ##' @param n The total number of markers. This is inferred when \code{x} is a
 ##'   \code{COMPASSResult}, and is unused in that case.
+##' @param markers A \code{character} specifying the markers for which to compute the score. Must match names in \code{markers()}.
 ##' @return A numeric vector of polyfunctionality scores.
 ##' @export
 ##' @examples
 ##' PolyfunctionalityScore(CR)
-PolyfunctionalityScore <- function(x, degree, n) {
+PolyfunctionalityScore <- function(x, degree, n,markers=NULL) {
   UseMethod("PolyfunctionalityScore")
 }
 
 ##' @rdname PolyfunctionalityScore
 ##' @export
-PolyfunctionalityScore.COMPASSResult <- function(x, degree, n) {
+PolyfunctionalityScore.COMPASSResult <- function(x, degree, n,markers=NULL) {
   degree <- x$fit$categories[, "Counts"]
   n <- ncol(x$fit$categories) - 1
-  x <- x$fit$mean_gamma
-  apply(x, 1, function(row) {
+  y <- x$fit$mean_gamma
+  pfs= apply(y, 1, function(row) {
     ## (2 / (n+1)) is a factor that normalized the score between 0 and 1
     sum(row * degree / choose(n, degree)) / n * (2 / (n + 1))
   })
+  if(!is.null(markers)){
+    if(!all(markers%in%markers(x))){
+      stop("Invalid marker names")
+    }
+    message("Computing a Polyfunctionality Score based on ",paste(markers,collapse=", "))
+    new_categories = unique(categories(x,FALSE)[,markers,drop=FALSE])
+    all_categories=categories(x,FALSE)[,markers,drop=FALSE]
+    dmat = as.matrix(pdist(new_categories,all_categories))
+    cat_indices = apply(dmat,1,function(y)which(y==0))
+    new_mean_gamma=sapply(cat_indices,function(i)apply(Gamma(x)[,i,],1,mean))
+    degree <- rowSums(new_categories)
+    n <- ncol(new_categories)
+    y <- new_mean_gamma
+    pfs= apply(y, 1, function(row) {
+      ## (2 / (n+1)) is a factor that normalized the score between 0 and 1
+      sum(row * degree / choose(n, degree)) / n * (2 / (n + 1))
+    })
+  }
+  return(pfs)
 }
 
 ##' @rdname PolyfunctionalityScore
 ##' @export
-PolyfunctionalityScore.default <- function(x, degree, n) {
+PolyfunctionalityScore.default <- function(x, degree, n,markers=NULL) {
   apply(x, 1, function(row) {
     ## (2 / (n+1)) is a factor that normalized the score between 0 and 1
     sum(row * degree / choose(n, degree)) / n * (2 / (n + 1))
