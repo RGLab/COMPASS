87cdb718b52dd35f24b849f5a0ee887b8d5519e9
m.jiang <m.jiang@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-08-05 21:43:48 +0000

    add validity check on children node supplied in 'mp'. close #50
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/COMPASS@119919 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ tests/testthat/test-GatingSet2COMPASS.R
@@ -0,0 +1,36 @@
+context("test COMPASSContainerFromGatingSet")
+dataDir <- system.file("extdata/gs_manual",package="flowWorkspaceData")
+library(flowWorkspace)
+gs <- load_gs(dataDir)
+gs1 <- clone(gs)
+sampleNames(gs1) <- "sample2.fcs"
+gs <- rbind2(GatingSetList(list(gs, gs1)))
+pd <- pData(gs)
+pd[["name"]] <- rownames(pd)
+pd[["PTID"]] <- 1
+pData(gs) <- pd
+test_that("COMPASSContainerFromGatingSet", {
+  cc <- COMPASSContainerFromGatingSet(gs, node = "CD8", mp = list("CD8/38+ DR+" = "DR"
+                                                            ,"CD8/38+ DR-" = "38")
+                                )
+  mat <- cc[["data"]][[1]]
+  expect_equal(colnames(mat), c("HLA-DR V500", "CD38 APC"))
+  expect_equal(nrow(mat), 5785)
+  expect_equal(cc[["counts"]][[1]], 14564)
+
+  #wrong subset
+  expect_error(cc <- COMPASSContainerFromGatingSet(gs, node = "CD8", mp = list("CD8/38+ DR+" = "DR"
+                                                                  ,"CD4/38+ DR-" = "38")
+                                      )
+               , "not the children")
+  #name column to be different from rownames
+  pData(gs)[["name"]] <- "sample"
+  cc <- COMPASSContainerFromGatingSet(gs, node = "CD8", mp = list("CD8/38+ DR+" = "DR"
+                                                                  ,"CD8/38+ DR-" = "38")
+  )
+  mat <- cc[["data"]][[1]]
+  expect_equal(colnames(mat), c("HLA-DR V500", "CD38 APC"))
+  expect_equal(nrow(mat), 5785)
+  expect_equal(cc[["counts"]][[1]], 14564)
+  
+})
